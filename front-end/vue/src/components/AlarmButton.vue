5a0ab50c633f967a23aa62b5207b32a7a26f8e47
Spencer Imbleau <spencer@imbleau.com>
2021-10-31 09:17:07 -0400

    Stylized alarm button

--- front-end/vue/src/components/AlarmButton.vue
+++ front-end/vue/src/components/AlarmButton.vue
@@ -1,12 +1,24 @@
 <template>
-  <button class="button-33">{{ label }}</button>
+  <button
+    v-bind:class="state.class"
+    v-bind:disabled="state.disabled"
+    class="button-33"
+  >
+    {{ state.label }}
+  </button>
 </template>
 
 <script>
+export const BUTTON_STATE = {
+  READY: { label: "Alarm!", class: "ready", disabled: false },
+  LOADING: { label: "Loading...", class: "loading", disabled: true },
+  FAILED: { label: "Failed. Try again?", class: "failed", disabled: true },
+  SUCCESS: { label: "Succeeded!", class: "success", disabled: true },
+};
 export default {
   name: "AlarmButton",
   props: {
-    label: String,
+    state: BUTTON_STATE,
   },
 };
 </script>
@@ -14,15 +26,9 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .button-33 {
-  background-color: #c2fbd7;
   width: 250px;
   height: 75px;
   border-radius: 100px;
-  box-shadow: rgba(44, 187, 99, 0.2) 0 -25px 18px -14px inset,
-    rgba(44, 187, 99, 0.15) 0 1px 2px, rgba(44, 187, 99, 0.15) 0 2px 4px,
-    rgba(44, 187, 99, 0.15) 0 4px 8px, rgba(44, 187, 99, 0.15) 0 8px 16px,
-    rgba(44, 187, 99, 0.15) 0 16px 32px;
-  color: green;
   cursor: pointer;
   display: inline-block;
   font-family: CerebriSans-Regular, -apple-system, system-ui, Roboto, sans-serif;
@@ -37,11 +43,43 @@ export default {
   touch-action: manipulation;
 }
 
-.button-33:hover {
-  box-shadow: rgba(44, 187, 99, 0.35) 0 -25px 18px -14px inset,
-    rgba(44, 187, 99, 0.25) 0 1px 2px, rgba(44, 187, 99, 0.25) 0 2px 4px,
-    rgba(44, 187, 99, 0.25) 0 4px 8px, rgba(44, 187, 99, 0.25) 0 8px 16px,
-    rgba(44, 187, 99, 0.25) 0 16px 32px;
+.ready {
+  background-color: #fbecc2;
+  box-shadow: rgba(235, 179, 27, 0.2) 0 -25px 18px -14px inset,
+    rgba(235, 179, 27, 0.15) 0 1px 2px, rgba(235, 179, 27, 0.15) 0 2px 4px,
+    rgba(235, 179, 27, 0.15) 0 4px 8px, rgba(235, 179, 27, 0.15) 0 8px 16px,
+    rgba(235, 179, 27, 0.15) 0 16px 32px;
+  color: goldenrod;
+}
+.ready:hover {
   transform: scale(1.25) rotate(-1deg);
+  box-shadow: rgba(235, 179, 27, 0.35) 0 -25px 18px -14px inset,
+    rgba(235, 179, 27, 0.25) 0 1px 2px, rgba(235, 179, 27, 0.25) 0 2px 4px,
+    rgba(235, 179, 27, 0.25) 0 4px 8px, rgba(235, 179, 27, 0.25) 0 8px 16px,
+    rgba(235, 179, 27, 0.25) 0 16px 32px;
+}
+.loading {
+  background-color: #c2c2c2;
+  box-shadow: rgba(44, 44, 44, 0.2) 0 -25px 18px -14px inset,
+    rgba(44, 44, 44, 0.15) 0 1px 2px, rgba(44, 44, 44, 0.15) 0 2px 4px,
+    rgba(44, 44, 44, 0.15) 0 4px 8px, rgba(44, 44, 44, 0.15) 0 8px 16px,
+    rgba(44, 44, 44, 0.15) 0 16px 32px;
+  color: grey;
+}
+.success {
+  background-color: #c2fbd7;
+  box-shadow: rgba(44, 187, 99, 0.2) 0 -25px 18px -14px inset,
+    rgba(44, 187, 99, 0.15) 0 1px 2px, rgba(44, 187, 99, 0.15) 0 2px 4px,
+    rgba(44, 187, 99, 0.15) 0 4px 8px, rgba(44, 187, 99, 0.15) 0 8px 16px,
+    rgba(44, 187, 99, 0.15) 0 16px 32px;
+  color: green;
+}
+.failed {
+  background-color: #fbc2c2;
+  box-shadow: rgba(187, 44, 44, 0.2) 0 -25px 18px -14px inset,
+    rgba(187, 44, 44, 0.15) 0 1px 2px, rgba(187, 44, 44, 0.15) 0 2px 4px,
+    rgba(187, 44, 44, 0.15) 0 4px 8px, rgba(187, 44, 44, 0.15) 0 8px 16px,
+    rgba(187, 44, 44, 0.15) 0 16px 32px;
+  color: red;
 }
 </style>
--- front-end/vue/src/views/Home.vue
+++ front-end/vue/src/views/Home.vue
@@ -2,17 +2,43 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <br />
-    <AlarmButton label="Alarm!" />
+    <AlarmButton
+      id="alarm_button"
+      v-bind:state="button_state"
+      v-on:click="call()"
+    />
   </div>
 </template>
 
 <script>
 import AlarmButton from "@/components/AlarmButton.vue";
+import { BUTTON_STATE } from "@/components/AlarmButton.vue";
 
 export default {
   name: "Home",
+  data: function () {
+    return {
+      button_state: BUTTON_STATE.READY,
+    };
+  },
   components: {
     AlarmButton,
+  },
+  methods: {
+    sleep: function (ms) {
+      // Expensive function
+      return new Promise((resolve) => setTimeout(resolve, ms));
+    },
+    call: async function () {
+      this.button_state = BUTTON_STATE.LOADING;
+      await this.sleep(2000); // Send the call
+      // Evaluate the result, show result
+      let success = Math.random() < 0.5 ? true : false;
+      this.button_state = success ? BUTTON_STATE.SUCCESS : BUTTON_STATE.FAILED;
+      // Revert to ready state
+      await this.sleep(2000); // Expensive function
+      this.button_state = BUTTON_STATE.READY;
+    },
   },
 };
 </script>
