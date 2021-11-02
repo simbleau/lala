d02043b0c87b8aa04289a9af63537bf703eca42f
Spencer Imbleau <spencer@imbleau.com>
2021-11-02 08:25:57 -0400

    Added mutations and getters to store

--- front-end/vue/src/store/store.js
+++ front-end/vue/src/store/store.js
@@ -2,12 +2,53 @@ import { createStore } from "vuex"
 
 const store = createStore({
     state: {
-        count: 0,
+        HISTORY_CAPACITY: 200,
+        servers: [],
+        responders: [],
+        history: [],
+    },
+    getters: {
+        get_servers: state => {
+            return state.servers;
+        },
+        get_num_servers: state => {
+            return state.servers.length
+        },
+        get_responders: state => {
+            return state.responders;
+        },
+        get_num_responders: state => {
+            return state.responders.length
+        },
+        get_history: state => {
+            return state.history;
+        },
     },
     mutations: {
-        increment(state) {
-            state.count++;
+        add_server(state, server) {
+            state.servers.push(server);
         },
+        remove_server(state, server) {
+            const index = state.servers.indexOf(server);
+            if (index > -1) {
+                state.servers.splice(index, 1);
+            }
+        },
+        add_responder(state, responder) {
+            state.responders.push(responder);
+        },
+        remove_responder(state, responder) {
+            const index = state.responders.indexOf(responder);
+            if (index > -1) {
+                state.responders.splice(index, 1);
+            }
+        },
+        add_history(state, log) {
+            state.history.push(log)
+            if (state.history.length > state.HISTORY_CAPACITY) {
+                state.history.shift();
+            }
+        }
     },
 })
 
