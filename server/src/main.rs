d423697dbe03d56feeecd832086bcf7dac5d69ad
Spencer Imbleau <spencer@imbleau.com>
2021-11-24 11:30:57 -0500

    Fixed mutual exclusion

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,12 @@
 #[macro_use]
 extern crate rocket;
+use std::sync::Mutex;
+
 use ringbuffer::RingBufferExt;
 use rocket::State;
 use rocket_client_addr::ClientAddr;
-use std::sync::Mutex;
 mod state;
-use state::Config;
+use state::AlarmState;
 
 #[catch(404)]
 fn not_found() -> &'static str {
@@ -18,31 +19,36 @@ fn internal_error() -> &'static str {
 }
 
 #[get("/status")]
-fn status(state: &State<Mutex<Config>>) -> String {
-    let config = state.lock().unwrap();
-    format!("{}", config.status())
+fn status(state: &State<Mutex<AlarmState>>) -> String {
+    let alarm = state.lock().unwrap();
+    format!("{}", alarm.status())
 }
 
 #[post("/signal")]
-fn signal(state: &State<Mutex<Config>>, client_addr: &ClientAddr) -> String {
-    let mut config = state.lock().unwrap();
-    config.signal(client_addr);
-    format!("{}", config.status())
+fn signal(
+    state: &State<Mutex<AlarmState>>,
+    client_addr: &ClientAddr,
+) -> String {
+    let mut alarm = state.lock().unwrap();
+    alarm.signal(client_addr);
+    format!("{}", alarm.status())
 }
 
 #[post("/silence")]
-fn silence(state: &State<Mutex<Config>>, client_addr: &ClientAddr) -> String {
-    let mut config = state.lock().unwrap();
-    config.silence(client_addr);
-    format!("{}", config.status())
+fn silence(
+    state: &State<Mutex<AlarmState>>,
+    client_addr: &ClientAddr,
+) -> String {
+    let mut alarm = state.lock().unwrap();
+    alarm.silence(client_addr);
+    format!("{}", alarm.status())
 }
 
 #[get("/history")]
-fn history(state: &State<Mutex<Config>>) -> String {
-    let config = state.lock().unwrap();
-    let history = config.get_history();
+fn history(state: &State<Mutex<AlarmState>>) -> String {
+    let alarm = state.lock().unwrap();
     let mut values = Vec::new();
-    for entry in history.iter().rev() {
+    for entry in alarm.history().entries().iter().rev() {
         values.push(serde_json::to_value(entry).unwrap());
     }
     serde_json::value::Value::Array(values).to_string()
@@ -51,7 +57,7 @@ fn history(state: &State<Mutex<Config>>) -> String {
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .manage(Mutex::new(Config::default()))
+        .manage(Mutex::new(AlarmState::default()))
         .mount("/", routes![status, signal, silence, history])
         .register("/", catchers![not_found, internal_error])
 }
--- server/src/state.rs
+++ server/src/state.rs
@@ -3,7 +3,81 @@ use ringbuffer::{ConstGenericRingBuffer, RingBufferWrite};
 use rocket::serde::Serialize;
 use rocket_client_addr::ClientAddr;
 
+pub struct AlarmState {
+    on: bool,
+    history: History,
+}
+
+impl AlarmState {
+    pub fn status(&self) -> bool {
+        self.on
+    }
+    pub fn signal(&mut self, client_addr: &ClientAddr) {
+        // Add history log
+        self.history.add(
+            &client_addr
+                .get_ipv4_string()
+                .unwrap_or(client_addr.get_ipv6_string()),
+            "signal",
+        );
+        // Turn on alarm
+        self.on = true;
+    }
+    pub fn silence(&mut self, client_addr: &ClientAddr) {
+        // Add history log
+        self.history.add(
+            &client_addr
+                .get_ipv4_string()
+                .unwrap_or(client_addr.get_ipv6_string()),
+            "silence",
+        );
+        // Turn off alarm
+        self.on = false;
+    }
+    pub fn history(&self) -> &History {
+        &self.history
+    }
+}
+
+impl Default for AlarmState {
+    fn default() -> Self {
+        AlarmState {
+            on: false,
+            history: History::new(),
+        }
+    }
+}
+
 const HISTORY_SIZE: usize = 2_usize.pow(10);
+
+pub struct History {
+    next_id: i32,
+    history: ConstGenericRingBuffer<HistoryEntry, HISTORY_SIZE>,
+}
+
+impl History {
+    fn new() -> Self {
+        History {
+            next_id: 0,
+            history: ConstGenericRingBuffer::new(),
+        }
+    }
+
+    pub fn entries(
+        &self,
+    ) -> &ConstGenericRingBuffer<HistoryEntry, HISTORY_SIZE> {
+        &self.history
+    }
+
+    pub fn add<'a>(&mut self, ip: &'a str, kind: &'a str) {
+        let id = self.next_id;
+        self.next_id += 1;
+        let ip = ip.to_owned();
+        let kind = kind.to_owned();
+        let date = Utc::now().format("%d%m%Y_%H-%M-%S").to_string();
+        self.history.push(HistoryEntry { id, ip, kind, date });
+    }
+}
 
 #[derive(Serialize)]
 pub struct HistoryEntry {
@@ -11,71 +85,4 @@ pub struct HistoryEntry {
     ip: String,
     kind: String,
     date: String,
-}
-
-impl HistoryEntry {
-    pub fn new<'a>(id: i32, ip: &'a str, kind: &'a str) -> Self {
-        let ip = ip.to_owned();
-        let kind = kind.to_owned();
-        let date = Utc::now().format("%d%m%Y_%H-%M-%S").to_string();
-        HistoryEntry { id, ip, kind, date }
-    }
-}
-
-pub struct Config {
-    on: bool,
-    id: i32,
-    history: ConstGenericRingBuffer<HistoryEntry, HISTORY_SIZE>,
-}
-
-impl Config {
-    pub fn status(&self) -> bool {
-        self.on
-    }
-    pub fn signal(&mut self, client_addr: &ClientAddr) {
-        // Add history
-        self.add_history(HistoryEntry::new(
-            self.id,
-            &client_addr
-                .get_ipv4_string()
-                .unwrap_or(client_addr.get_ipv6_string()),
-            "signal",
-        ));
-        // Increment ID
-        self.id += 1;
-        // Turn off alarm
-        self.on = true;
-    }
-    pub fn silence(&mut self, client_addr: &ClientAddr) {
-        // Add history
-        self.add_history(HistoryEntry::new(
-            self.id,
-            &client_addr
-                .get_ipv4_string()
-                .unwrap_or(client_addr.get_ipv6_string()),
-            "silence",
-        ));
-        // Increment ID
-        self.id += 1;
-        // Turn off alarm
-        self.on = false;
-    }
-    pub fn get_history(
-        &self,
-    ) -> &ConstGenericRingBuffer<HistoryEntry, HISTORY_SIZE> {
-        &self.history
-    }
-    pub fn add_history(&mut self, entry: HistoryEntry) {
-        self.history.push(entry);
-    }
-}
-
-impl Default for Config {
-    fn default() -> Self {
-        Config {
-            on: false,
-            id: 0,
-            history: ConstGenericRingBuffer::new(),
-        }
-    }
 }
