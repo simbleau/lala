d5eec705c1bc4733f24d0af6c69794962060eedd
Spencer Imbleau <spencer@imbleau.com>
2021-11-24 22:00:33 -0500

    Now translates loopback addr

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -5,5 +5,9 @@ pub const AUTHORITY_PORT: u16 = 8081;
 pub const ALARM_PORT: u16 = 8082;
 
 pub fn addr_to_string(addr: &rocket_client_addr::ClientAddr) -> String {
-    addr.get_ipv4_string().unwrap_or(addr.get_ipv6_string())
+    if addr.ip.is_loopback() {
+        "0.0.0.0".to_string()
+    } else {
+        addr.get_ipv4_string().unwrap_or(addr.get_ipv6_string())
+    }
 }
